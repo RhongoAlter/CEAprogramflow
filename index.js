154271395482b7732c1b53dbc83ff78c99cc380f
Alter-Ego Mark <mystic.rhongomyniad@gmail.com>
2023-07-30 17:43:36 +0800

    Added Fade in and Fade out effects to the rotate icon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const rotateIcon = document.getElementById('rotateIcon');
 
 // Set a timeout of 5 seconds (5000 milliseconds) to hide the element
 function timeout() { 
-    setTimeout(function(){rotateIcon.style.display='none'}, 5000);
+    setTimeout(function(){rotateIcon.style.display='none'}, 7000);
 }
 
 timeout();
