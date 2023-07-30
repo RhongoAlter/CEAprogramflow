78f3f5561b2ce439e8c7586f3ff439c7d1ae8149
Alter-Ego Mark <mystic.rhongomyniad@gmail.com>
2023-07-30 15:37:26 +0800

    Using keyframes for rotation

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 
 window.addEventListener("DOMContentLoaded", function () {
     const rotateIcon = document.getElementById("rotateIcon");
-    const icon = document.getElementById("icon");
+    
   
     // Check for device orientation change
     window.addEventListener("orientationchange", function () {
       // Check if the device is in portrait mode
-      if (window.innerHeight > window.innerWidth) {
+      if (window.matchMedia("(orientation: portrait)").matches) {
         // Show the rotate icon
         rotateIcon.style.display = "flex";
   
         // Add the CSS class to the icon for the rotation effect
-        icon.classList.add("rotate-this-icon");
+       
   
         // Hide the icon after a certain time (e.g., 5 seconds)
-        setTimeout(function () {
-          rotateIcon.style.display = "none";
-  
-          // Remove the CSS class to reset the rotation
-          icon.classList.remove("rotate-this-icon");
-        }, 5000); // Adjust the time as per your requirement (in milliseconds)
-      }
+        setTimeout(function () { rotateIcon.style.display = "none" }, 5000); // Adjust the time as per your requirement (in milliseconds)
+
+    }
     });
   });
   
