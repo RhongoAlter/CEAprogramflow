22ef337585c792ec0f5994f7099672f0d1704a23
Alter-Ego Mark <mystic.rhongomyniad@gmail.com>
2023-07-30 14:11:33 +0800

    Rotate Changes

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+
 window.addEventListener("DOMContentLoaded", function () {
     const rotateIcon = document.getElementById("rotateIcon");
+    const icon = document.getElementById("icon");
   
     // Check for device orientation change
     window.addEventListener("orientationchange", function () {
       // Check if the device is in portrait mode
       if (window.matchMedia("(orientation: portrait)").matches) {
         // Show the rotate icon
-        rotateIcon.style.display = "block";
-        rotateIcon.style.transform = 'rotate(-90deg)';
-        rotateIcon.style.transition = '2s';
+        rotateIcon.style.display = "flex";
+  
+        // Add the CSS class to the icon for the rotation effect
+        icon.classList.add("rotate-this-icon");
   
         // Hide the icon after a certain time (e.g., 5 seconds)
         setTimeout(function () {
           rotateIcon.style.display = "none";
+  
+          // Remove the CSS class to reset the rotation
+          icon.classList.remove("rotate-this-icon");
         }, 5000); // Adjust the time as per your requirement (in milliseconds)
       }
     });
